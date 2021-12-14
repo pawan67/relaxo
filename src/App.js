3c8fa76f1835e6f2804794a524981d3157d5f4c3
Pawan tamada <79091272+pawan67@users.noreply.github.com>
2021-12-14 13:46:15 +0530

    BUymeCofee

--- src/App.js
+++ src/App.js
@@ -142,6 +142,8 @@ function App() {
      
         {toggleBtn}
       </div>
+      
+
     </div>
   );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -8,6 +8,7 @@ import {
 function Footer() {
   return (
     <div className=" bg-[#000000]   w-screen  mt-32">
+      <a href="https://www.buymeacoffee.com/pawan.67"></a>
       <div className="container font-poppins text-xl   text-center mx-auto p-10 text-[#F0F5F9]">
         <p className=" font-poppins text-lg  inline">
           Made with &nbsp;
@@ -31,8 +32,14 @@ function Footer() {
             <AiFillTwitterCircle className="inline text-2xl ml-4" />
           </a>
         </p>
+        <a href="https://www.buymeacoffee.com/pawan.67">
+          <img
+            className=" rounded-2xl inline ml-5 mt-5 mx-auto w-36 h-full"
+            src="images/buymecofee.svg"
+            alt=""
+          />
+        </a>
       </div>
-      
     </div>
   );
 }
